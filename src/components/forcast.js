8ed0f1ba4d244f76d8aed9a52ba5a8f088371fca
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-08 07:36:19 +0530

    Initial commit

---
+++ src/components/forcast.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Forcast = (props) => {
+  return (
+    <div className="weather">
+      {props.country && props.city && (
+        <p>
+          Location:
+          {props.city}, {props.country}
+        </p>
+      )}
+      {props.temperature && (
+        <p>
+          Temperature:
+          {props.temperature}°C
+        </p>
+      )}
+      {props.humidity && (
+        <p>
+          Humidity:
+          {props.humidity}%
+        </p>
+      )}
+      {props.pressure && (
+        <p>
+          Pressure:
+          {props.pressure} pascal
+        </p>
+      )}
+      {props.icon && (
+        <img
+          src={`http://openweathermap.org/img/wn/${props.icon}.png`}
+          alt="weather icon"
+        />
+      )}
+      {props.description && (
+        <p>
+          Conditions:
+          {props.description}
+        </p>
+      )}
+      {props.error && <p>{props.error}</p>}
+    </div>
+  );
+};
+
+export default Forcast;
