86ea448e055b0976dd0c58a390c4800b6f4569a9
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-12-08 07:37:04 +0530

    code-added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 import Heading from "./components/heading";
 import Form from "./components/form";
 import Forcast from "./components/forcast";
 
 const api_key = "61ce79c32b969c4013d4d51c7ff533f3";
-
-
 
 class App extends React.Component {
   state = {
@@ -18,15 +16,17 @@ class App extends React.Component {
     icon: "",
     description: "",
     error: ""
-  }
+  };
 
-  getWeather = async(e) =>{
+  getWeather = async (e) => {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     e.preventDefault();
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${api_key}`);
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${api_key}`
+    );
     const response = await api_call.json();
-    if(city && country){
+    if (city && country) {
       this.setState({
         temperature: response.main.temp,
         city: response.name,
@@ -36,33 +36,34 @@ class App extends React.Component {
         icon: response.weather[0].icon,
         description: response.weather[0].description,
         error: ""
-      })
-    }else{
+      });
+    } else {
       this.setState({
         error: "Please fill out input field...."
-      })
+      });
     }
-  }
-  render(){
+  };
+  render() {
     return (
-      <div className="container"> 
+      <div className="container">
         <Heading />
         <div className="form">
-        <Form loadWeather={this.getWeather}/>
+          <Form loadWeather={this.getWeather} />
         </div>
         <div className="weather">
-        <Forcast temperature={this.state.temperature}
-        city={this.state.city}
-        country= {this.state.country}
-        humidity={this.state.humidity}
-        pressure = {this.state.pressure}
-        icon ={this.state.icon}
-        description = {this.state.description}
-        error = {this.state.error}
-         />
-         </div>
+          <Forcast
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            pressure={this.state.pressure}
+            icon={this.state.icon}
+            description={this.state.description}
+            error={this.state.error}
+          />
+        </div>
       </div>
-    )
+    );
   }
 }
 export default App;
